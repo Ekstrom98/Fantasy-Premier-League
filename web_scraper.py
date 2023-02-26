c2e8edc635943bd370aa9115cd209bc60d499f24
Viktor Ekström <viktorekstrom1998@gmail.com>
2023-02-26 18:18:27 +0100

    Accept cookies

--- web_scraper.py
+++ web_scraper.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Set Chrome to run in headless mode
 chrome_options = Options()
-chrome_options.add_argument('--headless')
+#chrome_options.add_argument('--headless')
 
 # Install and manage the Chrome driver executable
 service = Service(executable_path=ChromeDriverManager().install())
@@ -20,8 +22,17 @@ stats_url = "https://fantasy.premierleague.com/statistics"
 # Navigate to a webpage
 driver.get(stats_url)
 
+# Wait for the button to be clickable
+button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[@class='_2hTJ5th4dIYlveipSEMYHH BfdVlAo_cgSVjDUegen0F js-accept-all-close']"))
+)
+
+# Click the button
+button.click()
 # Print the title of the page
 print(driver.title)
+import time 
+time.sleep(4)
 
 # Close the browser
 driver.quit()
